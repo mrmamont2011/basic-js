291716326c1bd18c2fd62b84ce6e64f62c673140
Kotov Sergey <t9087620180@gmail.com>
2020-09-28 06:46:24 +0300

    fix two first tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,13 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  const sample = parseFloat(sampleActivity);
+  if(typeof sampleActivity !=='string' || isNaN(sample)) return false;
+    
+  if (sample <=0 || sample >15) return false;
+  
+  return Math.ceil(Math.log(MODERN_ACTIVITY/sample)/(0.693/HALF_LIFE_PERIOD));
+
+  
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,13 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function countCats(matrix) {
+        
+  if(!matrix == true) return 0;
+  let sum=0;
+  
+  for (let i=0; i<matrix.length;i++){
+             for(let j=0; j<(matrix[i].length); j++) if (matrix[i][j]=='^^') sum++;             
+          
+      
+      }
+    
+  return (sum);
+}
