c41bada8f05461b3c08f3629470757e5304a6968
Kotov Sergey <t9087620180@gmail.com>
2020-10-06 11:55:57 +0300

    add: task Chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,70 @@
 const CustomError = require("../extensions/custom-error");
 
+
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+  
+    array : [],
+
+    getLength() {
+      return this.array.length;
+    },
+    addLink(value) {
+      
+      if (value === undefined) {
+
+        this.array.push(' ');
+      } else {
+
+        this.array.push(`${value}`);
+      } 
+       
+      return this;
+    },
+
+    removeLink(pos) {
+            if ( (typeof(pos) !== 'number' ) || (pos < 1) || (pos > this.array.length) ) {
+              this.array = []; throw Error('Error number');
+            };
+                this.array.splice(pos-1, 1);
+     
+      return this;
+    },
+
+    reverseChain() {
+      if (this.array.length> 1) this.array.reverse();
+     
+      return this;
+    },
+    finishChain() {
+     
+      let str = '';
+      if (this.array.length === 0) { str = ''}
+       else {
+
+      this.array.forEach((el, ind, array) => {
+        if (array.length === 0) {
+           this.array = []; 
+           str = '';
+          }
+
+              if ((ind === 0) || (ind < (array.length-1))) 
+                { if (array.length ===1 ) {
+                  str = `${str}( ${el} )`
+                } else {
+                  str = `${str}( ${el} )~~`} 
+                } else  {
+                  str = `${str}( ${el} )`
+                }
+      });
+    }
+      this.array = [];
+      return str;
+    }
+
+  
+  
+ 
 };
 
 module.exports = chainMaker;
