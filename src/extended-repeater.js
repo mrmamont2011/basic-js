1492dae2f0e3e858da973fb68ca18735ff2af1e7
Kotov Sergey <t9087620180@gmail.com>
2020-11-05 21:24:08 +0300

    add: tasks extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+
+module.exports = function repeater(str, options) {
+  // throw new CustomError('Not implemented');
+  if (options.separator == undefined) options.separator = '+';
+
+  if (options.additionSeparator == undefined) options.additionSeparator = '|';
+
+  if (options.repeatTimes == undefined) options.separator = '', options.repeatTimes = 1;
   
+  if (options.additionRepeatTimes == undefined) options.additionSeparator = '', options.additionRepeatTimes = 1;
+
+  let addition = (`${options.addition}${options.additionSeparator}`).repeat(options.additionRepeatTimes);
+  let additions = addition.substr(0, addition.length - options.additionSeparator.length);
+
+  if (additions == 'undefined') additions = '';
+
+  let res = (`${str}${additions}${options.separator}`).repeat(options.repeatTimes);
+
+  let out = res.substr(0, res.length - options.separator.length);
+
+  return out;
+};
